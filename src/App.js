5ad0edaeef5368200d20fefaa5f8b618254f584a
pandoragear <andy.dang.it@gmail.com>
2021-08-08 19:16:38 -0500

    Add useState to count

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(4);
+
+  function decrementCount () {
+    setCount ( prevCount => prevCount - 1)
+  };
+
+  function incrementCount () {
+    setCount ( prevCount => prevCount + 1)
+  };
+
+
   return (
 <>
-    <button> - </button>
-    <span> 0 </span>
-    <button> + </button>
+    <button onClick = {decrementCount}> - </button>
+    <span> {count} </span>
+    <button onClick = {incrementCount} > + </button>
 </>
   );
 }
