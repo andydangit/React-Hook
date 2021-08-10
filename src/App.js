124d440e8edc98cbe195577c637184eaa867b628
Andy Dang <andy.dang.it@gmail.com>
2021-08-10 18:11:54 -0500

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-///////// Hook -- UseEffect
-
-import { useState, useEffect } from "react";
-import "./App.css";
+import React, {useState} from 'react'
 
 export default function App() {
-  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
 
- const handleResize = () => {
-  setwindowWidth(window.innerWidth)
- }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize)
-
-
-    // this will clean up the code  
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-
-
+  const[name, setName] = useState('')
 
   return (
-    <>
-<div> {windowWidth }
-</div>
-    </>
-  );
+          <input 
+      type="text"
+      value={name}
+      onChange={e => setName (e.target.value) }
+    />
+  )
 }
