abfcc3ac22aa0dd154eb43e71f0db2df2578b0e0
Andy Dang <andy.dang.it@gmail.com>
2021-08-09 20:44:15 -0500

    remove code

--- src/App.js
+++ src/App.js
@@ -4,26 +4,15 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
-  const [resourceType, SetResourceType] = useState("posts");
-  const [items, setItems] = useState([])
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setItems(json));
-  }, [resourceType]);
+
 
   return (
     <>
-      <div>
-        <button onClick={() => SetResourceType("posts")}> Posts </button>
-        <button onClick={() => SetResourceType("users")}> Users </button>
-        <button onClick={() => SetResourceType("comments")}> Comments </button>
-      </div>
-      <h1> {resourceType} </h1>
-      {items.map(item => {
-        return <pre> {JSON.stringify(item)} |</pre> 
-      })}
+<div> 
+
+</div>
     </>
   );
 }
