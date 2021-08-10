cfff04e5306c49582511c87223579330759241cf
Andy Dang <andy.dang.it@gmail.com>
2021-08-09 20:50:31 -0500

    add window resize useEffect

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ import "./App.css";
 export default function App() {
   const [windowWidth, setwindowWidth] = useState(window.innerWidth);
 
+ const handleResize = () => {
+  setwindowWidth(window.innerWidth)
+ }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+
+    // this will clean up the code  
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
 
 
   return (
     <>
-<div> 
-
+<div> {windowWidth }
 </div>
     </>
   );
