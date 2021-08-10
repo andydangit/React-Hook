3594a90c51c454575c6a9aa9bec608c3ee989de1
Andy Dang <andy.dang.it@gmail.com>
2021-08-10 18:27:26 -0500

    add useEffect to and value

--- src/Component/useLocalStorage.jsx
+++ src/Component/useLocalStorage.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function getSavedValue(key, initialValue){
- const savedValue =   JSON.parse( localStorage.getItem(key))
+ const savedValue =  JSON.parse( localStorage.getItem(key))
  if(savedValue) return savedValue
 
  if (initialValue instanceof Function) return initialValue()
@@ -12,6 +12,10 @@ export default function useLocalStorage(key, initialValue) {
 const [value, setValue] =useState(() => {
     return getSavedValue(key, initialValue)
 })
+
+useEffect (() => {
+    localStorage.setItem(key, JSON.stringify(value))
+}, [value]) 
 
 return [value,setValue]
 }
