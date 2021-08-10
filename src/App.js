ff2fdac43558afe4dfc9c031f54ab1134f709af7
Andy Dang <andy.dang.it@gmail.com>
2021-08-09 20:42:11 -0500

    add api and render map

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-///////// UseEffect  
+///////// Hook -- UseEffect
 
-import { useState } from 'react';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
 
 export default function App() {
-  const [resourceType, SetResourceType] = useState ('posts')
+  const [resourceType, SetResourceType] = useState("posts");
+  const [items, setItems] = useState([])
 
-
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
 
   return (
-<>
-<div> 
-  <button onClick={() => SetResourceType('posts')} > Posts </button>
-  <button onClick={() => SetResourceType('users')} > Users </button>
-  <button onClick={() => SetResourceType('comments')} > Comments </button>
-    </div> 
-    <h1> {resourceType} </h1>
-</>
-  )
+    <>
+      <div>
+        <button onClick={() => SetResourceType("posts")}> Posts </button>
+        <button onClick={() => SetResourceType("users")}> Users </button>
+        <button onClick={() => SetResourceType("comments")}> Comments </button>
+      </div>
+      <h1> {resourceType} </h1>
+      {items.map(item => {
+        return <pre> {JSON.stringify(item)} |</pre> 
+      })}
+    </>
+  );
 }
