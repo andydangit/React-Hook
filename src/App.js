72a449fe913452e53ad3f72ef2ba459839d3899d
pandoragear <andy.dang.it@gmail.com>
2021-08-11 23:36:29 -0500

    adding count to useState

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect} from "react";
 
 const App = () => {
 
+const [count, setCount] = useState(0);
 
+const increment = () => {
+  setCount (count + 1)
+}
 
   return (
     <div className="App">
-
+      {count}
+      <button onClick = {() => 
+      setCount (count + 1)}> Increment </button>
     </div>
   );
 }
