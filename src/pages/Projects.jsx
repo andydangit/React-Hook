e79500ee954846789833b42f85e663dc1029ad18
pandoragear <andy.dang.it@gmail.com>
2021-08-13 00:15:49 -0500

    ad useHistory

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 function Projects() {
-    return (
-        <div>
-            this is the project page
-        </div>
-    )
+  const history = useHistory();
+
+  return (
+    <div>
+      this is the project page
+      <button
+        onClick={() => {
+          history.push("/about");
+        }}
+      >
+        Go To The About
+      </button>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
