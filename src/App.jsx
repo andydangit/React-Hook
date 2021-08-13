9a11c00e39036fae618623d0ce356de39302fbda
pandoragear <andy.dang.it@gmail.com>
2021-08-12 23:54:41 -0500

    add color to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
   return(
+    <>
+    <div style ={{width: 100  + "vw", height: 80, backgroundColor: "lightblue"}}> </div>
   <Router>
     <Switch>
       <Route path="/" exact component={Home}/> 
       <Route path="/projects" exact component={Projects}/> 
       <Route path="/about" exact component={About}/> 
-      <Route path="*" exact component={About}/> 
+      <Route path="*" exact component={PageNotFound}/> 
     </Switch>
   </Router>
+  </>
   );
 }
 
