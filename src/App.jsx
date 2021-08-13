215705aca9093da6b0c281b3dbcd195656dc7b0c
pandoragear <andy.dang.it@gmail.com>
2021-08-12 23:33:17 -0500

    add router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <h1> test</h1>
-    </div>
-  )
+  return;
+  <Router>
+    <Switch>
+      <Route path=""/> 
+    </Switch>
+  </Router>;
 }
 
-export default App
-
+export default App;
