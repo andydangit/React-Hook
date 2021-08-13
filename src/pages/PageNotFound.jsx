fe886fdea3583928f882980e7cd0c5947fb2372c
pandoragear <andy.dang.it@gmail.com>
2021-08-13 00:23:04 -0500

    add uselocation

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 function PageNotFound() {
+
+    const location = useLocation();
+
     return (
         <div>
-            No such page exits
+            Sorry about that, the page {location.pathname} does not exist
         </div>
     )
 }
