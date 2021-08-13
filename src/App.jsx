a850a49b2c749da7c55027bc5d3b9a21954ef947
pandoragear <andy.dang.it@gmail.com>
2021-08-13 00:15:24 -0500

    add navbar link

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import './App.css'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import PageNotFound from './pages/PageNotFound'
 
+
 function App() {
   return(
     <>
-    <div style ={{width: 100  + "vw", height: 80, backgroundColor: "lightblue"}}> </div>
-  <Router>
+      <Router>
+    <div style ={{width: 100  + "vw", height: 80, backgroundColor: "lightblue"}}> 
+    <Link to="/"> Home </Link>
+    <Link to="/projects"> Projects </Link>
+    <Link to="/about"> About </Link>
+    </div>
+
     <Switch>
       <Route path="/" exact component={Home}/> 
       <Route path="/projects" exact component={Projects}/> 
