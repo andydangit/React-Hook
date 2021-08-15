4ba7c7a885a772836f2ca8f8324d264c031dd6af
pandoragear <andy.dang.it@gmail.com>
2021-08-15 16:58:40 -0500

    Add getJoke function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route,  Link, } from 'react-router-dom'
+// import {BrowserRouter as Router, Switch, Route,  Link, } from 'react-router-dom'
+import Axios from 'axios';
+
+
 
 function App() {
+
+  // const url = `https://official-joke-api.appspot.com/random_joke`;
+
+  const getJoke = () => {
+    Axios.get("https://official-joke-api.appspot.com/random_joke").then(
+      (response) => {
+      console.log(response);
+      }
+    );
+  }
+
+
   return (
     <div>
-      
+      <button onClick={getJoke}> get joke </button>
     </div>
   )
 }
 
 export default App
+
