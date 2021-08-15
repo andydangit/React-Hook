7bdb5bc967dc440b95c5a756b6a6230cf12272b1
pandoragear <andy.dang.it@gmail.com>
2021-08-15 17:04:40 -0500

    useState to store data and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import {BrowserRouter as Router, Switch, Route,  Link, } from 'react-router-dom'
 import Axios from 'axios';
 
@@ -8,10 +8,13 @@ function App() {
 
   // const url = `https://official-joke-api.appspot.com/random_joke`;
 
+  const [joke, setJoke] = useState("");
+
   const getJoke = () => {
     Axios.get("https://official-joke-api.appspot.com/random_joke").then(
       (response) => {
-      console.log(response);
+      setJoke(response.data.setup + ".... " + response.data.punchline)
+      
       }
     );
   }
@@ -20,6 +23,8 @@ function App() {
   return (
     <div>
       <button onClick={getJoke}> get joke </button>
+      <br/>
+      {joke}
     </div>
   )
 }
