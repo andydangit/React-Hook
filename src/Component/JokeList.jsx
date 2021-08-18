86ab340fc4874f134caeda7e0d9613dd0e87acc4
pandoragear <andy.dang.it@gmail.com>
2021-08-17 21:26:26 -0500

    add Axios to pull api

--- src/Component/JokeList.jsx
+++ src/Component/JokeList.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import JokeCard from './JokeCard';
 
 function JokeList() {
+    const url = "https://official-joke-api.appspot.com/random_joke"
+
+    const [joke, setJoke] = useState([]);
+
+    useEffect(() => { 
+  const getJoke = async() => {
+      const response = await Axios.get(`${url}`);
+      setJoke(response.data)
+  };
+getJoke();
+}, []);
+ 
+
+
+
+
+
     return (
         <div>
-            The List 
+           {joke.map((joke) => {
+               return <JokeCard joke={joke} /> 
+           })}
         </div>
     )
 }
