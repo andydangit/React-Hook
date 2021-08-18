2b50ae278b00be4ca681766adfd99dd96a858e5a
pandoragear <andy.dang.it@gmail.com>
2021-08-17 20:59:17 -0500

    inital files for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,11 @@
-import React, {useState} from 'react'
-// import {BrowserRouter as Router, Switch, Route,  Link, } from 'react-router-dom'
-import Axios from 'axios';
-
-
+import React from 'react';
 
 function App() {
-
-  // const url = `https://official-joke-api.appspot.com/random_joke`;
-
-  const [joke, setJoke] = useState("");
-
-  // Axios
-  // const getJoke = () => {
-  //   Axios.get("https://official-joke-api.appspot.com/random_joke").then(
-  //     (response) => {
-  //     setJoke(response.data.setup + ".... " + response.data.punchline)
-      
-  //     }
-  //   );
-  // }
-
-
-  // using fetch
-  const getJoke = () => {
-    fetch("https://official-joke-api.appspot.com/random_joke").then(
-      (response) => response.json()).then((data) => {
-        setJoke(data.setup + ".... " + data.punchline)
-      });
-  }
-
   return (
     <div>
-      <button onClick={getJoke}> get joke </button>
-      <br/>
-      {joke}
+      test
     </div>
   )
 }
 
 export default App
-
