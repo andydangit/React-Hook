46d9b8c47f915f0cf18156fa0b8f42506ce3b228
pandoragear <andy.dang.it@gmail.com>
2021-08-17 21:05:42 -0500

    add Router page in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import JokeList from "./Component/JokeList";
 
 function App() {
   return (
     <div>
-      test
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <JokeList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
