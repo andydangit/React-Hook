081cffe4c18574da1c34633a4b39ac39f36225b5
Andy Dang <andy.dang.it@gmail.com>
2021-09-08 18:28:29 -0500

    fix conflict error

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ import JokeList from "./Component/JokeList";
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <button onClick={getJoke}> get jokes </button>
-      <br/>
-      {joke}
-=======
       <Router>
         <Switch>
           <Route exact path="/">
@@ -17,13 +12,8 @@ function App() {
           </Route>
         </Switch>
       </Router>
->>>>>>> 5317c8e660101af9f878d8e0315392b729a5ede1
     </div>
   );
 }
 
-<<<<<<< HEAD
-export default App
-=======
 export default App;
->>>>>>> 5317c8e660101af9f878d8e0315392b729a5ede1
